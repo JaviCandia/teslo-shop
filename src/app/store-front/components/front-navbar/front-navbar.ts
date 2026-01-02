202797666639a3d936887bf3682e350e2cdd7613
Javier Candia <javi-candia@hotmail.com>
2026-01-01 18:31:26 -0600

    initial routes and components generated | tailwind and daysiUI added

---
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'front-navbar',
+  imports: [RouterLink],
+  templateUrl: './front-navbar.html',
+  styles: ``,
+})
+export class FrontNavbar {
+
+}
